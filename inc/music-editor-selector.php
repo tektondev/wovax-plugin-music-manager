5972971185bc600f9b96ca4a82646792f5484683
danbleile <danbleile@gmail.com>
2016-06-29 13:54:55 -0700

    JS timing Issue

--- inc/music-editor-selector.php
+++ inc/music-editor-selector.php
@@ -1,5 +1,5 @@
 
-<div id="wovax-mm-selector" style="display:none">
+<div id="wovax-mm-selector" style="top:-9999rem;position:absolute;">
 	<div>
     	<div id="wovax-music-selector-form">
         	<header>
